8b4e56ad0189ebee9f9516401ea762b88f320271
Gauravbio <93136782+Gauravbio@users.noreply.github.com>
2023-04-16 22:58:48 +0530

    Reservation

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,6 @@ export default async function Home() {
     <ClientOnly>
       <Container>
         <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-          <div>
               {
                 listings.map((listing)=> (
                   <ListingCard 
@@ -31,7 +30,6 @@ export default async function Home() {
                   />
                 ))
               }
-          </div>
         </div>
       </Container>
     </ClientOnly>
