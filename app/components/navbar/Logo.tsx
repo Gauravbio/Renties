ce163883a725e937b06af2ad4debb545a3f6b887
Gauravbio <93136782+Gauravbio@users.noreply.github.com>
2023-04-18 18:37:57 +0530

    Renties

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -5,9 +5,9 @@ import { useRouter } from "next/navigation";
 const Logo=()=> {
     const router=useRouter();
     return (
-        <div className="flex flex-row">
-            <Image onClick={()=> router.push('/')} alt="logo" className="hidden md:block cursor-pointer" height="100" width="100" src="/images/Renties.png" />
-            <h2>Renties</h2>
+        <div className="flex flex-row items-center justify-center">
+            <Image onClick={()=> router.push('/')} alt="logo" className="hidden md:block cursor-pointer" height="50" width="50" src="/images/Renties.png" />
+            <span className="font-bold">Renties</span>
         </div>
     )
 }
