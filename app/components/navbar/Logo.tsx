795a04ff8b6717b498a6ae73ddf69c31590c5752
Gauravbio <93136782+Gauravbio@users.noreply.github.com>
2023-04-18 18:27:11 +0530

    Logo change

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -5,7 +5,10 @@ import { useRouter } from "next/navigation";
 const Logo=()=> {
     const router=useRouter();
     return (
-        <Image onClick={()=> router.push('/')} alt="logo" className="hidden md:block cursor-pointer" height="100" width="100" src="/images/Renties.png" />
+        <div className="flex flex-row">
+            <Image onClick={()=> router.push('/')} alt="logo" className="hidden md:block cursor-pointer" height="100" width="100" src="/images/Renties.png" />
+            <h2>Renties</h2>
+        </div>
     )
 }
 
