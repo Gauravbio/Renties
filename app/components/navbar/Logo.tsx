487cf5597eb93253e7cf1970f7307f2c79586ff8
Gauravbio <93136782+Gauravbio@users.noreply.github.com>
2023-05-22 20:31:20 +0530

    logo

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -7,7 +7,7 @@ const Logo=()=> {
     return (
         <div className="flex flex-row items-center justify-center">
             <Image onClick={()=> router.push('/')} alt="logo" className="hidden md:block cursor-pointer" height="50" width="50" src="/images/Renties.png" />
-            <span className="font-bold">Renties</span>
+            <span className="font-bold hidden md:block">Renties</span>
         </div>
     )
 }
